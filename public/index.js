b64393ab201fb99cd751677d147724bdd68b0697
Marcus A. Schnake <marcus.schnake@gmail.com>
2022-06-02 15:15:39 -0400

    linked/added notes

--- public/index.js
+++ public/index.js
@@ -1,3 +1,9 @@
-"use strict";
-console.log("Go to hell world");
+import { log, } from "cool-shit-pkg";
+log("Go to hell world");
 //hello world is for the weak, be a man
+//number of paragraphs linking
+//Header size config function
+//P tag generation functions
+//NSF filter toggle linking
+//generate paragraph linking button
+//clear/reset text box button
